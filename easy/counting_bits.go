b1ae3546e49d17e1837ea0b82373fa2f53ff53eb
trungluongwww <trungluongwww@gmail.com>
2024-02-09 22:49:41 +0700

    add

--- easy/counting_bits.go
+++ easy/counting_bits.go
@@ -1,1 +1,30 @@
-package easy
+package main
+
+import "fmt"
+
+func dfFunc(n int) int {
+	count := 0
+	for n > 0 {
+		if n%2 == 1 {
+			count++
+		}
+
+		n = n / 2
+	}
+
+	return count
+}
+
+func countBits(n int) []int {
+	df := make([]int, n+1)
+
+	for i := 1; i <= n; i++ {
+		df[i] = dfFunc(i)
+	}
+
+	return df
+}
+
+func main() {
+	fmt.Println(3 & 1)
+}
--- easy/pascal_triangle.go
+++ easy/pascal_triangle.go
@@ -1,1 +1,32 @@
 package main
+
+import "fmt"
+
+func generate(numRows int) [][]int {
+	res := make([][]int, numRows)
+
+	for i := range res {
+		res[i] = make([]int, i+1)
+	}
+
+	res[0][0] = 1
+
+	for i := 1; i < numRows; i++ {
+		n := len(res[i])
+		res[i][0] = 1
+		res[i][n-1] = 1
+		for j := 1; j < n/2+n%2; j++ {
+
+			v := res[i-1][j-1] + res[i-1][j]
+
+			res[i][j] = v
+			res[i][n-j-1] = v
+		}
+	}
+
+	return res
+}
+
+func main() {
+	fmt.Println(generate(5))
+}
